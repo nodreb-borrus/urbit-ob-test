35a5a81ae46002e9b52d3dfddcebfda3cd9fd30e
~nodreb-borrus <nodreb@borr.us>
2023-11-09 04:05:05 -0700

    Handle defunct processes

--- src/main.rs
+++ src/main.rs
@@ -69,6 +69,9 @@ fn main() -> io::Result<()> {
                     println!("MISMATCH: {}", line);
                 }
             }
+
+            node_cmd.wait()?;
+
             Ok(WorkReport {
                 start,
                 end,
